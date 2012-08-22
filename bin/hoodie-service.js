41c2366d06acfdbdaaad4eccf18d4d90ca3c2f40
Jan Lehnardt <jan@apache.org>
2012-08-22 19:08:41 +0200

    express yourself

--- bin/hoodie-service.js
+++ bin/hoodie-service.js
@@ -1,9 +1,55 @@
 #!/usr/bin/env node
-var http = require('http');
+var express = require("express");
+var fs = require("fs");
+var cons = require("consolidate");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var app = express();
 
-console.log('Server running at http://127.0.0.1:1337/');
+var config = read_config({
+  apps: {}
+});
+
+console.log("Configuration: %j", config);
+
+app.engine("html", cons.hogan);
+app.set("view engine", "html");
+app.set("views", "web/views");
+
+app.get("/", function(req, res) {
+  if(!has_apps()) {
+    // var apps = get_app_info(config.apps);
+    res.render("list_of_apps.html", {
+      apps: "apps"
+    });
+  } else {
+    res.render("first_run.html");
+  }
+});
+
+app.listen(1235);
+console.log("Listening on port 1235");
+
+
+// util
+
+function read_config(defaults)
+{
+  try {
+    var config = JSON.parse(fs.readFileSync("/Users/jan/.hoodie.json"));
+    for(var name in defaults) {
+      if(!config[name]) {
+        config[name] = defaults[name];
+      }
+    }
+    return config;
+ } catch(e) {
+  console.log(e);
+  return defaults;
+ }
+}
+
+function has_apps()
+{
+  console.log(JSON.stringify(config.apps));
+  return JSON.stringify(config.apps) !== "{}";
+}
